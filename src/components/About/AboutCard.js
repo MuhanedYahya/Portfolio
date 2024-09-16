7f81bb48abedaac36f2ff574461349b0139f2d9c
MuhanedYahya <yahya.muhaned@gmail.com>
2024-09-17 00:30:31 +0300

    about page modified

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,17 +10,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "left" }}>
 
-            Hi Everyone, I've been a Linux enthusiast for many years, gaining expertise in managing Linux systems
-            and troubleshooting errors across various distributions such as Debian, CentOS, and Arch.
-            My passion for Virtualization and DevOps deepened during internships and my university graduation project.
-            <br></br>
-            <br></br>
-            Currently, I am a <FaBriefcase /> DevOps Engineer at{" "}
-            <a href="https://frostline.games/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "#007bff" }}>
-             	 Frostline Games
-            </a>, specializing in the gaming industry. With a solid background in Networking, Linux, and Containerization, I am dedicated to continuous learning 
-            and skill development to drive innovation in this dynamic field.
-          <br></br>
+          I've been a Linux enthusiast for many years, gaining expertise in managing Linux systems and troubleshooting errors across various distributions. Currently, I am a DevOps Engineer at Frostline Games, specializing in the gaming industry. With a solid background in Networking, Linux, and Containerization, I am dedicated to continuous learning and skill development to drive innovation in this dynamic field.
           <br></br>
           <br></br>
           <br></br>
